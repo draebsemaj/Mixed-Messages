75efb3459c9f1990db17bb6bc31fa827217e0a97
James Beard <85023075+draebsemaj@users.noreply.github.com>
2021-06-16 16:11:53 -0500

    Complete javascript message generator

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Static arrays of messages
 const danger = ['An Ambitious Organization', 'Planar Forces', 'Arcane Enemies', 'Hordes', 'A Cursed Place'];
 const doom = ['Tyranny', 'Pestilence', 'Destruction', 'Usurptation', 'Impoverishment', 'Rampant Chaos'];
-const portentWitness = ['A Local', 'A Traveller', 'An Animal', 'A Criminal', 'A Monster', 'A Place'];
+const portentWitness = ['A local', 'A traveller', 'An animal', 'A criminal', 'A monster', 'A place'];
 const portentVerb = ['has found', 'has seen', 'has hidden', 'is dead because of', 'is alive because of', 'was caught with'];
 const portent = ['a secret cache of stolen supplies', 'an area with unnatural properties', 'a gem that shares knowledge', 'a trail with hundreds of footprints', 'a glow rising from the west' ];
-const stakes = [];
+const stakes = ['What will become of our friends?', 'What will we have to sacrifice?', 'Will we ever return?', 'Are we capable enough?', 'Are we the bad guys?', 'Can things return to normal?'];
 
 // Random selection function
 const random = array => {
@@ -14,3 +14,4 @@ const random = array => {
 console.log(`The danger facing the players is that of ${random(danger)}.`);
 console.log(`The ultimate doom is one of ${random(doom)}.`);
 console.log(`${random(portentWitness)} ${random(portentVerb)} ${random(portent)}.`)
+console.log(`The players must ask "${random(stakes)}".`)
